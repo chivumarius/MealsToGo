8f67275f6f8433f6ea580d629534264fe44d8810
Chivu Marius <chivumarius@yahoo.com>
2022-04-08 13:49:46 +0300

    Geocoding API → Integrating

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,6 @@
-const liveHost = "https://us-central1-mealstogo-b2612.cloudfunctions.net";
-const localHost = "http://localhost:5001/mealstogo-b2612/us-central1";
+
+const liveHost = "https://us-central1-mealstogo-7519b.cloudfunctions.net";
+const localHost = "http://localhost:5001/mealstogo-7519b/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
