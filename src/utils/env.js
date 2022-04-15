935a2b505765171a34ad569d25c8e55700fb59b2
Chivu Marius <chivumarius@yahoo.com>
2022-04-15 10:50:53 +0300

    Optimizing Switching 'Live Host' to 'Local Host' → for Android

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,10 @@
+import { Platform } from "react-native";
+
 const liveHost = "https://us-central1-mealstogo-7519b.cloudfunctions.net";
 const localHost = "http://localhost:5001/mealstogo-7519b/us-central1";
 
-export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const host = isDevelopment ? localHost : liveHost;
+export const isAndroid = Platform.OS === "android";
+export const isDevelopment = process.env.NODE_ENV === "development";
+export const isMock = false;
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
