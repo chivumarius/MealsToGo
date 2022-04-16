111a87d1ef5ddcadb832e50c59bdb36cb6b542d4
Chivu Marius <chivumarius@yahoo.com>
2022-04-16 10:46:32 +0300

    Stripe Token → for Creating 'Token Request'

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
+import createStripe from "stripe-client";
+
+const stripe = createStripe(
+  "pk_test_51JwTW7BwpUBC3co7evNpjDWPGS4YV0XiKdoz88jpMyQhuC06etVgnoOWU6jbU7nFrpiBJ4tdkl93SySyFRu6z1q700VZjGCTz1"
+);
 
 export const CreditCardInput = () => {
-  const onChange = (formData) => {
+  const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
     console.log(isIncomplete);
+    const card = {
+      number: "4242424242424242",
+      exp_month: "02",
+      exp_year: "24",
+      cvc: "244",
+      name: "Mo",
+    };
+    const info = await stripe.createToken({ card });
+    console.log(info);
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
